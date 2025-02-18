9034f82f0864163e0fa140774818a7f9db2bea7c
hari0409 <hari626007@gmail.com>
2025-02-19 01:50:50 +0530

    Create Rest Routes

---
+++ src/main/java/com/example/library_management_system/routes/AuthorRoute.java
@@ -0,0 +1,10 @@
+package com.example.library_management_system.routes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/author")
+public class AuthorRoute {
+
+}
